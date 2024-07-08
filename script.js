78ce02540f9b9bc468000b9035851dfdc9e4c1f2
Devvishnoi6 <devvishnoi.0618@gmail.com>
2024-07-08 16:36:16 +0530

    commit

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ const Cityinput = document.querySelector(".city-input");
 const API_KEY="f128a0926011690876a20df4ebe8c9ba"; //API key for openweathermap API
 
 
+const createWeatherCard = (weatherItem) =>{
+    return` <li class="card">
+                        <h3>(2024-07-05)</h3>
+                        <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather-icon">
+                        <h4>Temp: 21°C</h4>
+                        <h4>Wind: 4.31 M/S</h4>
+                        <h4>Humidity: 74%</h4>
+                    </li>`;
+}
 const getWeatherDetails=(CityName,lat,lon)=>{
     const WeatherAPI=`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch( WeatherAPI).then(res=> res.json()).then(data=>{
-        console.log(data);
-
         //Filter the forecasts get only one forecast per day.
         const uniqueForecastDays = [];
-        data.list.filter(forecast =>{
+        const fivedaysforecast = data.list.filter(forecast =>{
             const forecastDate = new Date(forecast.dt_txt).getDate();
             if(!uniqueForecastDays.includes(forecastDate)){
                 return uniqueForecastDays.push(forecastDate);
             }
 
-        })
+        });
+        console.log(fivedaysforecast);
+        fivedaysforecast.forEach(weatherItem => {
+            createWeatherCard(weatherItem);
+        });
     }).catch(()=>{
         alert("An error occured while fetching the weather forecast!");
     });
